ff120219604921903cf7d8977fcef48c47171e3b
Steve Christian <steven.tr.christian@gmail.com>
2021-10-31 17:53:16 +0100

    reaction timer initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="startGame" :disabled="isPlaying">Start game</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="score" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+    }
+  },
+  components: { Block, Results },
+  methods: {
+    startGame() {
+      this.delay = 1000 + Math.random() * 3000  
+      this.isPlaying = !this.isPlaying
+      this.score = null;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+    }
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
+
+button {
+  padding: 1em 2em;
+  border-radius: 0.5em;
+  border: none;
+  background: #0faf87;
+  color: #eee;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1em;
+  letter-spacing: 1px;
+  cursor: pointer;
+  text-transform: uppercase;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+
+button:active {
+  position: relative;
+  top: 2px;
+}
+
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <p class="results">
+        Reaction Time: {{ score }} ms
+    </p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 100) {
+            this.rank = "The Fastest Draw in the West"
+        } else if (this.score < 250) {
+            this.rank = "Lightning Quick"
+        } else if (this.score < 400) {
+            this.rank = "Rapid Reflexes"
+        } else {
+            this.rank = "Snail's Pace"
+        }
+    }
+}
+</script>
+
+<style>
+    .results {
+        display: block;
+        margin: 50px auto;
+    }
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
